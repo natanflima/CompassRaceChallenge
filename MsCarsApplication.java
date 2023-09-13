268cdbad877f4171f10f8ed0465b4550bc056c5c
natanflima <natanflima@hotmail.com>
2023-09-13 01:58:13 -0300

    Updating code

--- MsCarsApplication.java
+++ MsCarsApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MsCarsApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(MsCarsApplication.class, args);
 	}
 
